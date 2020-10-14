65e2330fce145fa04130b3821dcef7de0fd5bb96
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-14 22:41:00 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,9 +45,7 @@ class Song
   end 
   
   def self.find_or_create_by_name(song)
-    if !self.find_by_name(song) 
-      song = Song.new 
-    end 
+    self.find_by_name(song) || self = Song.new 
   end
   
 
