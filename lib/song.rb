43d0b184c9edafc8c2a678880d1b383e8e8ab780
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-14 22:52:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,8 +45,8 @@ class Song
   end 
   
   def self.find_or_create_by_name(song)
-    self.find_by_name(song) || 
-    binding.pry 
+    self.find_by_name(song) || song = Song.new(song)
+    @@all << song 
   end
   
 
