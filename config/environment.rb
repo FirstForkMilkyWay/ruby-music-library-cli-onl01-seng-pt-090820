ffae280ea7cde55f7b7e76a26c417f2df1f81c0a
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-10-15 00:42:34 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,15 @@ Bundler.require
 
 module Concerns
   module Findable 
-  end 
+        def find_by_name(name)
+      all.find{|a| a.name == name}
+    end
+    
+    def find_or_create_by_name(name)
+      find_by_name(name) || create(name)
+    end
+    
+  end
 end
 
 require_all 'lib'
